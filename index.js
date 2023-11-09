4a5eeb1d9991905c0043d1b4cf51bf0d56548b15
AndresFLK <andrescrptycol@gmail.com>
2023-11-08 18:00:20 -0600

    Final Project

--- index.js
+++ index.js
@@ -6,16 +6,33 @@ import express from "express";
 // 2. Create an express app and set the port number.
 const app = express();
 const port = 3000;
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
 // 3. Use the public folder for static files.
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 
 
 app.get("/", (req, res) => {
-   
-        res.render("index.ejs");
-  });
+    res.render("index.ejs");
+});
+
+app.post("/search-name", async(req,res) =>{
+    const pokeName = req.body.PokemonName;
+    console.log(req.body)
+    try {
+        const result = await axios.get(apiUrl + pokeName);
+        console.log(result.data)
+        res.render("index.ejs", { content: result.data });
+      } catch (error) {
+        res.render("index.ejs", { content: error.response.data });
+      }
+    
+  
+});
+  
 
 
 
